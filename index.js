503356af5969c5acd166f23e67698512a93491cd
anaelisaq <anaelisa.abs@gmail.com>
2023-02-26 18:17:08 -0300

    api for studies about pure nodejs

---
+++ index.js
@@ -0,0 +1,50 @@
+const { randomUUID } = require('crypto')
+const http = require('http')
+
+const users = []
+const server = http.createServer((request, response) => {
+
+    if (request.url === "/users") {
+        if (request.method === 'GET') {
+            return response.end(JSON.stringify(users))
+        }
+        
+        if (request.method === 'POST') {
+            request.on('data', (data) => {
+                const dataUser = JSON.parse(data)
+                const user = {
+                    id: randomUUID(),
+                    ...dataUser
+                }
+
+                users.push(user)
+            }).on('end', () => {
+                return response.end(JSON.stringify(users))
+            })
+        }
+    }
+
+    if (request.url.startsWith("/users/")) {
+        if (request.method === 'PUT') {
+            const url = request.url
+            const splitURL = url.split("/")
+
+            const idUser = splitURL[2]
+            
+            const userIndex = users.findIndex(user => user.id === idUser)
+            
+            request.on('data', (data) => {
+                const dataUser = JSON.parse(data)
+
+                users[userIndex] = {
+                    id: idUser,
+                    ...dataUser
+                }
+            }).on('end', () => {
+                return response.end(JSON.stringify(users))
+            })
+        }
+    }
+})
+
+server.listen(8000, () => console.log('server is running on port 8000'))
